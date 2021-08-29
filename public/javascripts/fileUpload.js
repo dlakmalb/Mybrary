eb30b49f4739be19e83d7f842c45050ef2fa9b57
Dhananjaya Bulathsinghala <dhananjaya.lakmal@eleanorcare.co.uk>
2021-08-29 14:27:42 +0530

    Finish project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,15 +1,39 @@
-// Register all the plugins
-FilePond.registerPlugin(
-    FilePondPluginFileEncode,
-    FilePondPluginImageResize,
-    FilePondPluginImagePreview,
-);
+const rootStyles = window.getComputedStyle(document.documentElement);
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150,
-});
+if (
+    rootStyles.getPropertyValue('--book-cover-width-large') != null &&
+    rootStyles.getPropertyValue('--book-cover-width-large') !== ''
+) {
+    ready();
+} else {
+    document.getElementById('main-css').addEventListener('load', ready);
+}
 
-// Turn all file input elements into ponds
-FilePond.parse(document.body);
+function ready() {
+    const coverWidth = parseFloat(
+        rootStyles.getPropertyValue('--book-cover-width-large')
+    );
+
+    const coverAspectRatio = parseFloat(
+        rootStyles.getPropertyValue('--book-cover-aspect-ratio')
+    );
+
+    const coverHeight = coverWidth / coverAspectRatio;
+
+
+    // Register all the plugins
+    FilePond.registerPlugin(
+        FilePondPluginFileEncode,
+        FilePondPluginImageResize,
+        FilePondPluginImagePreview,
+    );
+
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverAspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight,
+    });
+
+    // Turn all file input elements into ponds
+    FilePond.parse(document.body);
+}
